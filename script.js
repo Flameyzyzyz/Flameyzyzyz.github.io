408ca83e13fb690e2f557531eab29d9827b1451a
Flameyzyzyz <91034606+Flameyzyzyz@users.noreply.github.com>
2022-08-28 21:11:01 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.left = "0%";
+    document.getElementById("openbtn").style.transition = "0.5s";
+    document.getElementById("openbtn").style.left = "-100%";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.left = "-100%";
+    document.getElementById("openbtn").style.transition = "0.5s";
+    document.getElementById("openbtn").style.left = "-5px";
+  }
