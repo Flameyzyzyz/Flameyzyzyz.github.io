09ccb79cb9c637697275bb74b11d3493850dc6d2
Flameyzyzyz <91034606+Flameyzyzyz@users.noreply.github.com>
2022-08-29 23:51:09 -0400

    More Fixing

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("mySidepanel").style.left = "0%";
-    document.getElementById("openbtn").style.transition = "0.5s";
-    document.getElementById("openbtn").style.left = "-100%";
+    // document.getElementById("sidepanel").style.transition = "0.5s";
+    // document.getElementById("menubtn").style.transition = "0.5s";
+    // document.getElementById("menubtn").style.zIndex = "4";
+    if (document.getElementById("sidepanel").style.left == "0%"){
+        document.getElementById("menubtn").style.left = "0%";
+        // setTimeout(function (){
+            document.getElementById("sidepanel").style.left = "-100%";
+        // }, 100)
+    } else {
+        document.getElementById("sidepanel").style.left = "0%";
+        // setTimeout(function (){
+            document.getElementById("menubtn").style.left = "188px";
+        // }, 100)
+    }
 }
-  
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-    document.getElementById("mySidepanel").style.left = "-100%";
-    document.getElementById("openbtn").style.transition = "0.5s";
-    document.getElementById("openbtn").style.left = "-5px";
-}
-  
